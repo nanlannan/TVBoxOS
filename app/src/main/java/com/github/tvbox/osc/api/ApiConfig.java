d3b905c70ac38f3fab6f9d7e9fa0a428827cbb14
nanlannan <162283931+nanlannan@users.noreply.github.com>
2024-03-05 15:52:33 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -127,7 +127,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "clan://localhost/tvboxTW/蜘蛛/c仓库接口.txt");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
